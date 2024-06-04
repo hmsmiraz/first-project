d5c9e178f64b97ad0ed3444cd8f1c7804bd7fcbb
hmsmiraz <hmsmiraz64729@gmail.com>
2024-06-05 01:11:48 +0600

    correction student model

--- src/app/modules/students/student.model.ts
+++ src/app/modules/students/student.model.ts
@@ -143,7 +143,7 @@ const studentSchema = new Schema<TStudent, StudentModel>(
 );
 // virtuals
 studentSchema.virtual('fullName').get(function () {
-  return `${this.name.firstName} ${this.name.middleName} ${this.name.lastName} `;
+  return `${this?.name?.firstName} ${this?.name?.middleName} ${this?.name?.lastName} `;
 });
 
 // Query middleware
